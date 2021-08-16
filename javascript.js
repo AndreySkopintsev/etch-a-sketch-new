7a35c4d73ef478e56212dfdfc1e741bfcd372f77
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2021-08-16 12:20:03 +0300

    added testing mouseover event to squares

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 const grid = document.getElementById('grid')
+let squares
+
+//Functions
 
 function fillGrid(){
     for(let i=0;i<256;i++){
@@ -6,6 +9,17 @@ function fillGrid(){
         div.classList.add('square')
         grid.appendChild(div)
     }
+    squares = document.querySelectorAll('.square')   
 }
 
+
+
 fillGrid()
+
+//Event listeners
+
+squares.forEach(square => {
+    square.addEventListener('mouseover',(e)=>{
+        square.classList.toggle('red')
+    })
+})
